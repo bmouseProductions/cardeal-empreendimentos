ba5961c6d37a921e98c6a23e42abcf9e48930974
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-08-17 10:49:42 -0300

    erro build consertado

--- src/components/buttonUp/index.tsx
+++ src/components/buttonUp/index.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp } from "@fortawesome/fontawesome-free-solid";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 export const ButtonUp = () => {
   return (
