acf7d60d3fa09384d2c55b439d6c5f511fc89ee5
beno1234 <benolopesdias@gmail.com>
2023-08-28 13:15:44 -0300

    whats

--- src/components/whats/whats.tsx
+++ src/components/whats/whats.tsx
@@ -5,7 +5,7 @@ const WhatsAppButton: React.FC = () => {
   return (
     <div className="fixed bottom-4 left-4 bg-green-500 hover:bg-green-600 text-white rounded-full p-3 shadow-lg z-10">
       <a
-        href="https://wa.me/5555349964400?text=Ol%C3%A1%2C+vim+atrav%C3%A9s+do+site+e+gostaria+de+receber+algumas+informa%C3%A7%C3%B5es."
+        href="https://wa.me/5534996440078?text=Ol%C3%A1%2C+vim+atrav%C3%A9s+do+site+e+gostaria+de+receber+algumas+informa%C3%A7%C3%B5es."
         target="_blank"
         rel="noopener noreferrer"
       >
