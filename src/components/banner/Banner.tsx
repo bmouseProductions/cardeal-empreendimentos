a83d217668b04cab96f37d73bcfd127d1e0993d8
bmouse1 <designers@bmouseproductions.com>
2024-01-09 14:26:32 -0300

    finished

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,33 +1,32 @@
 import Lottie  from 'react-lottie-player';
 
 import logo from '/logo-cardeal-branco.webp'
-import outdoor from '/OutdoorCardeal.webp'
 import swipeUp from '../../assets/swipeUp.json'
-interface PropsBanner {
-    page: string | null;
-}
+import { FaWhatsapp } from "react-icons/fa";
 
-export default function Banner( { page }:PropsBanner ){
+export default function Banner(){
     return (
-        <section className="w-full min-h-screen lg:min-h-[530px]  bg-banner relative">
-            <div className='flex flex-col-reverse items-center lg:flex-row lg:min-h-screen lg:gap-10'>
-                <div className='lg:w-1/2 h-full lg:min-h-[530px] flex flex-col justify-end items-center'>
+        <section className='bg-banner2'>
+            <div className="w-full min-h-screen lg:min-h-[530px]  bg-banner bg-opacity-75 relative">
 
-                    <img src={logo} className='w-full max-w-[150px] sm:max-w-[300px] xl:max-w-[550px] mt-5 lg:mb-20' alt="" />
-                    <h1 className='mb-20 mt-24 lg:mt-10 w-full lg:pl-10 text-center lg:text-start text-4xl lg:text-5xl 2xl:text-7xl text-white '>{page}</h1>
+            
+                <div className='flex flex-col-reverse items-center lg:flex-row lg:min-h-screen lg:gap-10'>
+                    <div className='lg:w-1/2 h-full lg:min-h-[530px] flex flex-col  items-center'>
 
+                        <img src={logo} className='w-full max-w-[150px] sm:max-w-[300px] xl:max-w-[550px] mt-5 lg:mb-20' alt="" />
+                        <a href="" target='_blank' className='py-3 px-10 flex items-center gap-5 text-2xl font-semibold text-white rounded animate-bounce bg-[#193592] '>
+                            <FaWhatsapp size="40px" /> Entre em contato 
+                        </a>
+                    </div>
                 </div>
-                <div className='w-1/2 h-full flex items-end '>
-                    <img src={outdoor} className='mt-10 lg:mt-0' alt="" />
+                <div className='absolute bottom-10 left-[20%] md:left-[35%] lg:hidden'>
+                    <Lottie
+                        play
+                        loop
+                        animationData={swipeUp}
+                        className='w-[200px] '
+                    />
                 </div>
-            </div>
-            <div className='absolute bottom-10 left-[20%] md:left-[35%] lg:hidden'>
-                <Lottie
-                    play
-                    loop
-                    animationData={swipeUp}
-                    className='w-[200px] '
-                />
             </div>
         </section>
     )
--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,15 +1,16 @@
 //import HeaderCarousel from "../../components/Header";
 import Footer from "../../components/Footer";
 import About from "../../components/about";
-import Banner from "../../components/Banner/Banner";
+//import Banner from "../../components/Banner/Banner";
 import Page2 from "../About/Page2";
 
 const Sobre = () => {
   return (
     <div className="bg-gray-300 ">
+      {/*
       <Banner
         page="Sobre"
-      />
+      />*/}
       <About />
       <Page2 />
       <Footer />
