4117b31b944ed053106b6322bec8aa4221c9d598
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-01-10 15:22:25 -0300

    finished

--- src/pages/Empreendimentos/Empreendimentos.tsx
+++ src/pages/Empreendimentos/Empreendimentos.tsx
@@ -1,70 +1,31 @@
 import React from 'react';
 import { FixedSizeList } from 'react-window';
 
+
 //import Banner from "../../components/banner/Banner";
-import Skill from "../../components/Skill";
 /*
 interface EmpreedimentosProps {
   name: string;
 }*/
 
 const Empreendimentos2 = () => {
-  const numberOfDivs = 29; // Total de divs que você quer renderizar
+  const numberOfDivs = 15; // Total de divs que você quer renderizar
 
-  const sectionTitles = [
-    "",
-    "Residencial Gontijo",
-    "Jardim America",
-    "Jardim Vitória",
-    "Residencial Manancial",
-    "Nova Floresta I, II, III E IV",
-    "Jardim Paraíso",
-    "Jardim Vitória",
-    "Belo Horizonte II",
-    "Santa Rita de Cássia",
-    "Residencial Amazonas",
-    "Residencial Vista Alegre",
-    "Jardim Vitória",
-    "Residencial Peluzzo",
-    "Nossa Senhora das Valias",
-    "Alta Villa",
-    "Residencial dos Jacarandás",
-    "Novo Paraíso I e II",
-    "Jardim Botânico",
-    "Belo Horizonte",
-    "Jardim Vitória",
-    "Jardim Vitória",
-    "Jardim Vitória II",
-    "Jardim Vitória I E II",
-    "Nossa Senhora das Lourdes",
-    "Santa Maria",
-    "Residencial Morada Nova IV",
-    "Jardim Vitória",
-    "Jardim Natal I",
-    "Versol Costa",
-  ];
+  
 
   const Row = ({ index, style }: { index: number; style: React.CSSProperties }) => (
     <div>
-      <div style={style} className="flex flex-col lg:flex-row justify-center border-b-4 border-black bg-[#02050b]">
-        <div className="lg:w-full 2xl:w-full max-w-[850px] ">
-          <img src={`/FotosCardeal(${index + 1}).webp`} className="h-full " alt="" />
+      <div style={style} className=" flex flex-col lg:flex-row justify-around border-b-4 border-black bg-[#02050b]">
+        <div className="flex">
+          <img src={`/FotosCardeal(${index * 2 + 1}).webp`} className=" w-full" alt="" />
         </div>
-        <div className=" bg-[#02050b] opacity-70">
-          <div className='lg:w-full 2xl:w-full'>
-            <Skill
-            title={sectionTitles[index + 1]}
-            ruas={10}
-            terra={15}
-            esgoto={30}
-            pluvial={20}
-          />
-          </div>
-          
+        <div className="flex">
+          <img src={`/FotosCardeal(${index * 2 + 2}).webp`} className=" w-full" alt="" />
         </div>
       </div>
     </div>
   );
+  
   
 
   return (
@@ -76,9 +37,9 @@ const Empreendimentos2 = () => {
       
       <div className='md:hidden'>
         <FixedSizeList
-          height={800} // Defina a altura desejada da lista
+          height={850} // Defina a altura desejada da lista
           itemCount={numberOfDivs}
-          itemSize={750} // Defina a altura de cada item
+          itemSize={500} // Defina a altura de cada item
           width="100%"
           className=' h-screen'
         >
